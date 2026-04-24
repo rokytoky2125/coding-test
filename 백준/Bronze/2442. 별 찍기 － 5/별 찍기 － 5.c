91c15a929e4fef5c801314f478ee05f75f9a0614
rokytoky <117971543+rokytoky2125@users.noreply.github.com>
2026-04-24 16:30:24 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for (int i = 1; i < N+1; i++)
+    {
+        for (int j = 0; j < N - i; j++)
+            printf(" ");
+        for (int j = 0; j < 2 * i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+}
