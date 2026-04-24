274b7ba446f46dfe8162bffa0bd45564c5dc0072
rokytoky <117971543+rokytoky2125@users.noreply.github.com>
2026-04-24 16:37:40 +0900

    [Bronze V] Title: 검증수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,10 +1,7 @@
-def func (nums):
-    s = 0
-    for i in nums:
-        s = s + (i**2)
-    r = s % 10
-    return r
-
-nums = list(map(int, input().split()))
-r = func(nums)
+lst = list(map(int, input().split()))
+s = 0
+r = 0
+for i in lst:
+    s += i ** 2
+r = s % 10
 print(r)
