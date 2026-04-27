705b52c971af84df9a32cc9b483b3ad03b128d22
rokytoky <117971543+rokytoky2125@users.noreply.github.com>
2026-04-27 23:35:59 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int ary[10];
+
+void dfs(int depth, int start)
+{
+    if (depth == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << ary[i] << " ";
+        cout << endl;
+        return;
+    }
+
+    for (int i = start; i <= n; i++)
+    {
+        ary[depth] = i;
+        dfs(depth + 1, i + 1);
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    dfs(0, 1);
+}
